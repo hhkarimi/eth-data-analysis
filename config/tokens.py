f03ed2e405aa9d8ea241f4e5a09152df30f68b0f
hhkarimi <hussain.h.karimi@gmail.com>
2021-08-28 16:34:20 -0400

    Set tokens to only LINK for data pull

--- config/tokens.py
+++ config/tokens.py
@@ -1,7 +1,3 @@
-
-# top eth accounts by balance: https://etherscan.io/accounts
-
-
 tokens = {
 #     'USDC': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
 #     'UNI': '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
