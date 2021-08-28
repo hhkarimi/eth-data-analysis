11bb2172a4c50051b3c99388f0618b0e467e6b1a
hhkarimi <hussain.h.karimi@gmail.com>
2021-08-28 16:29:49 -0400

    Updated addresses

---
+++ config/tokens.py
@@ -0,0 +1,9 @@
+
+# top eth accounts by balance: https://etherscan.io/accounts
+
+
+tokens = {
+#     'USDC': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+#     'UNI': '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+    'LINK': '0x514910771af9ca656af840dff83e8264ecf986ca',
+}
